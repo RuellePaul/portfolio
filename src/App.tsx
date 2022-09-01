d60319aa2a3b3280c36eb51118a7830e83ee1b3e
Paul Ruelle <paul.ruelle@widmee.com>
2022-09-01 15:05:10 +0200

    add Portfolio skeleton

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,12 @@
-import Universe from 'src/components/Universe.tsx';
+import Universe from 'src/components/Universe';
+import Portfolio from 'src/components/Portfolio';
 
 function App() {
     return (
         <>
             <Universe />
 
-            <main
-                style={{
-                    width: '100vw',
-                    background: 'transparent',
-                    zIndex: 10,
-                    position: 'absolute',
-                    top: '200vh',
-                    left: 0,
-                    height: 2000,
-                    backdropFilter: 'invert(1)'
-                }}
-            >
-                ...
-            </main>
+            <Portfolio />
         </>
     );
 }

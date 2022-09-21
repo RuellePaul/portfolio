80d043f92024eb5924b650c8407ce59007a24033
Paul Ruelle <paul.ruelle@widmee.com>
2022-09-21 18:08:53 +0200

    Revert "add smooth scrollbar, bind to universe"
    This reverts commit 0b267ebc85573464192443b0ef4867a1c626f059.

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,12 @@
 import Universe from 'src/components/Universe';
 import Portfolio from 'src/components/Portfolio';
-import Scrollbar from 'react-smooth-scrollbar';
 
 function App() {
     return (
         <>
             <Universe />
 
-            <Scrollbar damping={0.15}>
-                <Portfolio />
-            </Scrollbar>
+            <Portfolio />
         </>
     );
 }

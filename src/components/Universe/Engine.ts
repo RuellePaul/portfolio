e2a92837c375d779b2712739dde4f89eb7ebf9f3
Paul Ruelle <paul.ruelle@widmee.com>
2022-09-21 18:15:23 +0200

    clean code

--- src/components/Universe/Engine.ts
+++ src/components/Universe/Engine.ts
@@ -1,5 +1,5 @@
 import * as THREE from 'three';
-import {PCFSoftShadowMap, PerspectiveCamera, ReinhardToneMapping, WebGLRenderer} from 'three';
+import {PCFSoftShadowMap, PerspectiveCamera, ReinhardToneMapping, Vector3, WebGLRenderer} from 'three';
 import OpeningScene from 'src/components/Universe/scenes/OpeningScene';
 
 class Engine {
@@ -11,26 +11,35 @@ class Engine {
     constructor() {
         this.renderer = new WebGLRenderer({antialias: true});
         this.renderer.toneMapping = ReinhardToneMapping;
-        this.renderer.toneMappingExposure = Math.pow(1.5, 4.0);
+        this.renderer.toneMappingExposure = Math.pow(2, 4);
         this.renderer.shadowMap.type = PCFSoftShadowMap;
         this.renderer.shadowMap.enabled = true;
 
-        // 💡 Turn lights on/off
-        // this.renderer.setClearColor(0x000000);
-
         this.renderer.setPixelRatio(window.devicePixelRatio);
         this.renderer.setSize(window.innerWidth, window.innerHeight);
-
-        this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 10000);
 
         window.addEventListener('resize', this.resize);
 
         this.scene = new OpeningScene();
 
+        const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 10000);
+        this.camera = camera;
+        function moveCamera() {
+            const scrollY = Math.abs(document.body.getBoundingClientRect().top);
+            const height = document.body.clientHeight - window.innerHeight;
+
+            const targetPosition = new Vector3(0, 0, -100);
+
+            camera.position.x = (scrollY / height) * targetPosition.x;
+            camera.position.y = (scrollY / height) * targetPosition.y;
+            camera.position.z = (scrollY / height) * targetPosition.z;
+        }
+
         // Animation Loop
         const animate = () => {
             requestAnimationFrame(animate);
             this.scene.update();
+            moveCamera();
             this.renderer.render(this.scene, this.camera);
         };
         animate();

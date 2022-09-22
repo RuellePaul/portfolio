dd479c612799bbb4cd2c85556679bfcfa4acc33c
Paul Ruelle <paul.ruelle@widmee.com>
2022-09-22 13:52:06 +0200

    add SectionsContext

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,47 @@
-import React, {FC} from 'react';
-import Universe from 'src/components/Universe';
+import React, {FC, useLayoutEffect, useRef} from 'react';
 import Scrollbar from 'react-smooth-scrollbar';
 import {Box, CssBaseline} from '@mui/material';
+import Universe from 'src/components/Universe';
 
-const Section: FC = ({children}) => (
-    <Box sx={{border: 'solid 2px red', height: '200vh', color: 'white', fontSize: 200}}>{children}</Box>
-);
+import {SectionsContext, SectionsProvider, useSections} from 'src/store/Sections';
+
+const Section: FC<{order: number}> = ({order, children}) => {
+    const ref = useRef<HTMLElement>();
+
+    const {sections, setSections} = useSections();
+
+    useLayoutEffect(() => {
+        if (sections.length === 0) setSections((sections) => [...sections, {height: ref.current!.clientHeight, order}]);
+    }, []);
+
+    return <Box ref={ref}>{children}</Box>;
+};
 
 function App() {
     return (
-        <>
+        <SectionsProvider>
             <CssBaseline enableColorScheme />
 
             <Universe />
 
+            <SectionsContext.Consumer>
+                {(value) => (
+                    <pre style={{position: 'fixed', color: 'white'}}>{JSON.stringify(value.sections, null, 4)}</pre>
+                )}
+            </SectionsContext.Consumer>
+
             <Scrollbar damping={0.1}>
-                <Section>1</Section>
-                <Section>2</Section>
-                <Section>3</Section>
+                <Section order={1}>
+                    <Box sx={{border: 'solid 2px red', height: 3000}} />
+                </Section>
+                <Section order={2}>
+                    <Box sx={{border: 'solid 2px orange', height: 2000}} />
+                </Section>
+                <Section order={3}>
+                    <Box sx={{border: 'solid 2px white', height: 4000}} />
+                </Section>
             </Scrollbar>
-        </>
+        </SectionsProvider>
     );
 }
 
---
+++ src/store/Sections.tsx
@@ -0,0 +1,39 @@
+import React, {createContext, Dispatch, FC, ReactNode, SetStateAction, useContext, useState} from 'react';
+
+interface Section {
+    height: number;
+    order: number;
+}
+
+export interface SectionsContextValue {
+    sections: Section[];
+    setSections: Dispatch<SetStateAction<Section[]>>;
+}
+
+interface SectionsProviderProps {
+    children?: ReactNode;
+}
+
+export const SectionsContext = createContext<SectionsContextValue>({
+    sections: [],
+    setSections: () => {}
+});
+
+export const SectionsProvider: FC<SectionsProviderProps> = ({children}) => {
+    const [sections, setSections] = useState<Section[]>([]);
+
+    return (
+        <SectionsContext.Provider
+            value={{
+                sections: sections,
+                setSections: setSections
+            }}
+        >
+            {children}
+        </SectionsContext.Provider>
+    );
+};
+
+export const SectionsConsumer = SectionsContext.Consumer;
+
+export const useSections = (): SectionsContextValue => useContext(SectionsContext);

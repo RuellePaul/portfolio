61943f594f024105ddca4bc906cb72f50ae801ff
Paul Ruelle <paul.ruelle@widmee.com>
2022-09-23 13:54:58 +0200

    fixes

--- src/components/sections/Projects/Projects.jsx
+++ src/components/sections/Projects/Projects.jsx
@@ -1,8 +1,24 @@
 import React from 'react';
-import {Box} from '@mui/material';
+import {Box, Typography} from '@mui/material';
+import labeling_images from '../../../assets/videos/labeling_images.mp4';
 
 function Projects() {
-    return <Box sx={{height: 5000, background: 'transparent', backdropFilter: 'invert(1)'}} />;
+    return (
+        <Box sx={{height: 5000, background: 'transparent', backdropFilter: 'invert(1)'}}>
+            <Box sx={{py: 200}}>
+                <Typography variant="h1">Projects</Typography>
+            </Box>
+            <Box sx={{maxWidth: 300, padding: 4}}>
+                <video
+                    src={labeling_images}
+                    autoPlay
+                    draggable={false}
+                    loop
+                    muted
+                />
+            </Box>
+        </Box>
+    );
 }
 
 export default Projects;
--- src/components/sections/Skills/Skills.tsx
+++ src/components/sections/Skills/Skills.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import {Box} from '@mui/material';
 
 function Skills() {
-    return <Box sx={{border: 'dashed 5px orange', height: 5000}} />;
+    return <Box sx={{height: 5000}} />;
 }
 
 export default Skills;

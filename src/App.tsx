94f0cc34cce3ad23de89ac49a9e017fe67a440ba
Paul Ruelle <paul.ruelle@widmee.com>
2022-09-26 19:26:14 +0200

    WIP routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,79 +1,8 @@
-import React, {FC, useLayoutEffect, useRef} from 'react';
-import Scrollbar from 'react-smooth-scrollbar';
-import {Box, CssBaseline} from '@mui/material';
-import Universe from 'src/components/Universe';
-import {SectionsProvider, useSections} from 'src/store/Sections';
-import {Hero, Projects, Skills} from 'src/components/sections';
-import {Path} from 'src/components/Universe/utils/FlightPath';
-
-const Section: FC<{paths: Path[]}> = ({paths, children}) => {
-    const ref = useRef<HTMLElement>();
-
-    const {sections, setSections} = useSections();
-
-    useLayoutEffect(() => {
-        if (sections.length === 0) setSections((sections) => [...sections, {height: ref.current!.clientHeight, paths}]);
-    }, []);
-
-    return <Box ref={ref}>{children}</Box>;
-};
+import React from 'react';
+import Portfolio from 'src/views/Portfolio';
 
 function App() {
-    return (
-        <SectionsProvider>
-            <CssBaseline enableColorScheme />
-
-            <Universe />
-
-            <Scrollbar damping={0.1}>
-                <Section
-                    paths={[
-                        {
-                            type: 'position',
-                            value: {z: 1},
-                            start: 0,
-                            end: 1
-                        },
-                        {
-                            type: 'fov',
-                            value: 120,
-                            start: 0,
-                            end: 1
-                        },
-                        {
-                            type: 'rotation',
-                            value: {x: Math.PI / 2},
-                            start: 0.6,
-                            end: 1
-                        }
-                    ]}
-                >
-                    <Hero />
-                </Section>
-                <Section paths={[]}>
-                    <Projects />
-                </Section>
-                <Section
-                    paths={[
-                        {
-                            type: 'fov',
-                            value: 120,
-                            start: 0,
-                            end: 1
-                        },
-                        {
-                            type: 'offset',
-                            value: {z: 200},
-                            start: 0,
-                            end: 1
-                        }
-                    ]}
-                >
-                    <Skills />
-                </Section>
-            </Scrollbar>
-        </SectionsProvider>
-    );
+    return <Portfolio />;
 }
 
 export default App;

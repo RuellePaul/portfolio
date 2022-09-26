6349206f3f960bc166b0c8e0ac9349cd03181b10
Paul Ruelle <paul.ruelle@widmee.com>
2022-09-26 19:39:29 +0200

    add router

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,27 @@
 import React from 'react';
 import Portfolio from 'src/views/Portfolio';
+import {BrowserRouter, Navigate, Route, Routes} from 'react-router-dom';
 
 function App() {
-    return <Portfolio />;
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route
+                    path="/"
+                    element={<Portfolio />}
+                />
+                <Route
+                    path="*"
+                    element={
+                        <Navigate
+                            replace
+                            to="/"
+                        />
+                    }
+                />
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;

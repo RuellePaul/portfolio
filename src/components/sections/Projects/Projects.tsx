fabf3e471a31e40f090702e2769b9dc2f98a081e
paulruelle <paul.ruelle@widmee.com>
2023-04-18 02:10:50 +0200

    Add melodymaster

--- src/components/sections/Projects/Projects.tsx
+++ src/components/sections/Projects/Projects.tsx
@@ -16,6 +16,7 @@ import ArrowRight from '@mui/icons-material/KeyboardArrowRight';
 
 interface Project {
     name: string;
+    url?: string;
     stack: string[];
     description: ReactNode;
     github_url: string;
@@ -25,6 +26,17 @@ interface Project {
 }
 
 const PROJECTS: Project[] = [
+    {
+        name: 'Melody Master',
+        stack: ['typescript', 'react', 'python'],
+        description:
+            'Datatensor is a web application to prepare and manage image datasets for object detection, using an existing dataset or creating one from scratch.',
+        github_url: 'https://github.com/ThomasRoudil/melody-master',
+        image: 'melodymaster.png',
+        legend: 'melodymaster.io screens',
+        overlay_image: null,
+        url: 'https://melodymaster.io'
+    },
     {
         name: 'Datatensor',
         stack: ['typescript', 'react', 'python'],
@@ -54,9 +66,9 @@ const PROJECTS: Project[] = [
                 <br />
                 Give it a try on{' '}
                 <Link
-                    component="a"
-                    href="https://www.metacareers.com/profile/coding_puzzles/"
-                    target="_blank"
+                    component='a'
+                    href='https://www.metacareers.com/profile/coding_puzzles/'
+                    target='_blank'
                 >
                     metacareers.com/profile/coding_puzzles
                 </Link>
@@ -83,7 +95,8 @@ const PROJECTS: Project[] = [
 const GithubIcon: FC = () => {
     return (
         <SvgIcon>
-            <path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z" />
+            <path
+                d='M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z' />
         </SvgIcon>
     );
 };
@@ -101,22 +114,22 @@ const Project: FC<{project: Project}> = ({project}) => {
             }}
         >
             <Typography
-                variant="h3"
-                color="textPrimary"
+                variant='h3'
+                color='textPrimary'
                 gutterBottom
             >
                 {project.name}
             </Typography>
             <Stack
                 spacing={1}
-                direction="row"
+                direction='row'
             >
                 {project.stack.map((technology) => (
                     <Chip
                         key={technology}
                         label={
                             <Typography
-                                variant="overline"
+                                variant='overline'
                                 fontWeight={400}
                             >
                                 {technology}
@@ -135,8 +148,8 @@ const Project: FC<{project: Project}> = ({project}) => {
             </Stack>
 
             <Typography
-                variant="body1"
-                color="textPrimary"
+                variant='body1'
+                color='textPrimary'
                 sx={{my: 3, maxWidth: 650}}
             >
                 {project.description}
@@ -145,14 +158,14 @@ const Project: FC<{project: Project}> = ({project}) => {
             <img
                 src={`/static/images/${project.image}`}
                 alt={project.image}
-                width="100%"
+                width='100%'
             />
 
             <Typography
-                variant="caption"
-                component="p"
-                color="textSecondary"
-                align="center"
+                variant='caption'
+                component='p'
+                color='textSecondary'
+                align='center'
             >
                 {project.legend}
             </Typography>
@@ -164,19 +177,23 @@ const Project: FC<{project: Project}> = ({project}) => {
             >
                 <Button
                     endIcon={<ArrowRight />}
-                    variant="contained"
+                    variant='contained'
+                    onClick={() => {
+                        if (project.url)
+                            window.open(project.url, '_blank');
+                    }}
                 >
                     Learn more
                 </Button>
-                <Tooltip title="Open in Github">
+                <Tooltip title='Open in Github'>
                     <Button
-                        component="a"
+                        component='a'
                         href={project.github_url}
-                        target="_blank"
-                        rel="noopener noreferrer"
+                        target='_blank'
+                        rel='noopener noreferrer'
                         endIcon={<GithubIcon />}
-                        size="large"
-                        variant="outlined"
+                        size='large'
+                        variant='outlined'
                     >
                         {project.name}
                     </Button>
@@ -195,7 +212,7 @@ const Project: FC<{project: Project}> = ({project}) => {
                     <img
                         src={`/static/images/${project.overlay_image}`}
                         alt={project.overlay_image}
-                        height="100%"
+                        height='100%'
                     />
                 </Box>
             )}
@@ -206,12 +223,12 @@ const Project: FC<{project: Project}> = ({project}) => {
 function Projects() {
     return (
         <Container
-            maxWidth="lg"
+            maxWidth='lg'
             sx={{p: '0px !important'}}
         >
             <Box height={1000} />
             <Container
-                maxWidth="md"
+                maxWidth='md'
                 sx={{margin: 'initial', minHeight: 5000}}
             >
                 {PROJECTS.map((project) => (
